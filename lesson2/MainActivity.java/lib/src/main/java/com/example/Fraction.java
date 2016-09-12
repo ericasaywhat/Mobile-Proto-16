925ac55890397ae0b17753ac5c6595a8fe83239c
Erica Lee <erica.lee@students.olin.edu>
2016-09-12 15:09:58 -0400

    Turning in hw

---
+++ lesson2/MainActivity.java/lib/src/main/java/com/example/Fraction.java
@@ -0,0 +1,54 @@
+package com.example;
+
+/**
+ * Created by erica on 9/11/16.
+ */
+public class Fraction {
+
+    private int numerator;
+    private int denominator;
+
+    public Fraction(int numerator, int denominator) {
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+
+    public static int gcd(int m, int n) {
+        if(m>n) {
+            if(n==0) {
+                return m;
+            } else {
+                return gcd(n,m%n);
+            }
+        } else if (n>m) {
+            if(m==0) {
+                return n;
+            } else {
+                return gcd(m,n%m);
+            }
+        }else {
+            return m;
+        }
+    }
+
+    public Fraction simplify() {
+        int numGCD = gcd(numerator, denominator);
+        numerator = numerator/numGCD;
+        denominator = denominator/numGCD;
+        Fraction simpleFrac= new Fraction(numerator,denominator);
+        return simpleFrac;
+
+    }
+
+    public Fraction add(Fraction f2) {
+        int numerator = this.numerator*f2.denominator+f2.numerator*denominator;
+        int denominator = this.denominator*f2.denominator;
+        Fraction simpleAddedFrac = new Fraction(numerator, denominator);
+        return simpleAddedFrac.simplify();
+    }
+
+    public String toString() {
+        return Integer.toString(numerator) + "/" + Integer.toString(denominator);
+    }
+
+}
