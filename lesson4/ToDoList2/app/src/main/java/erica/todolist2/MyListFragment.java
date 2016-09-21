67e3ece6ee5d798c5066be3d1b3343bd73844834
David Abrahams <davidabrahams@sbcglobal.net>
2016-09-21 17:07:08 -0400

    adding comments and fixing code

--- lesson4/ToDoList2/app/src/main/java/erica/todolist2/MyListFragment.java
+++ lesson4/ToDoList2/app/src/main/java/erica/todolist2/MyListFragment.java
@@ -3,9 +3,11 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AlertDialog;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -45,6 +47,16 @@ public class MyListFragment extends Fragment {
 
         ButterKnife.bind(this,mainView);
         listView.setAdapter(adapter);
+
+        // You should use this instead of putting listeners on the individual rows. This will allow
+        // you to keep track of which element you clicked on (using the position field)
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                ListAdapter.dialogMaker(taskList, view, position, adapter);
+                adapter.notifyDataSetChanged();
+            }
+        });
 
         newTask.setOnClickListener(new View.OnClickListener() {
            @Override
